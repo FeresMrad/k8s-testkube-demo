b22d91e48817d699af7c81a66e42b76055b82ce4
feres <feresmrad98@gmail.com>
2026-03-08 21:07:02 +0100

    add product detail page

---
+++ src/ui/demo-ui/src/app/pages/product-details/product-details.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product.model';
+
+@Component({
+  selector: 'app-product-detail',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterLink,
+    MatCardModule,
+    MatChipsModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatIconModule
+  ],
+  templateUrl: './product-detail.component.html',
+  styleUrl: './product-detail.component.scss'
+})
+export class ProductDetailComponent implements OnInit {
+  product: Product | null = null;
+  loading = true;
+  notFound = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')!;
+    this.productService.getById(id).subscribe({
+      next: (data) => {
+        this.product = data;
+        this.loading = false;
+      },
+      error: () => {
+        this.loading = false;
+        this.notFound = true;
+      }
+    });
+  }
+}
